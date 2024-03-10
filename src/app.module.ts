9470e8589886da5595b70254a0d9ae61919da926
Rosalito gabriel <rosalitogabriel@solaireresort.com>
2024-03-10 14:12:08 +0800

    Added Code Base for JWT Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HealthModule } from './health/health.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`${process.env.MONGODB_CONN}`),
     HealthModule,
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
